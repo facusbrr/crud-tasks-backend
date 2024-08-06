3a8f13df9293bdfa7cd06df5dd701b324bb4b3c4
Gayoso Facundo <facusbrr@gmail.com>
2024-08-05 23:37:09 -0300

    Se realizo el controller createTasks para poder crear tareas facilmente

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -5,19 +5,23 @@ const ctrl = {};
 ctrl.obtenerTasks = async(req, res) =>{
     const connection = await connectDB();
     const [results] = await connection.query('SELECT * FROM tasks');
+    connection.end();
     return res.json(results);
 }
 // Crear
 ctrl.createTasks = async (req, res) => {
-    const {title, description, isComplete} = req.body;
-
-    title.trim() || description.trim('') ? null : res.status(400).json('Faltan datos');
-    const connection = await connectDB();
-    await connection.query('INSERT INTO TASKS(title, description, isComplete) VALUES (?,?,?)', [title, description, isComplete]);
-    res.json('Tarea creada');
+    const { title, description, isComplete } = req.body;
+    if (!title.trim() || !description.trim()) {
+        return res.status(400).json({ message: 'Hay datos vacíos' });
+    } else if(isComplete !== 0 && isComplete !== 1){
+        return res.status(400).json({ message: 'isComplete Debe ser 0 o 1'})
+    } else {
+        const connection = await connectDB();
+        await connection.query('INSERT INTO TASKS(title, description, isComplete) VALUES (?,?,?)', [title, description, isComplete]);
+        connection.end();
+        return res.json({message: 'Tarea completa'})
+    }
 }
-
-
 // Editar
 
 // Actualizar 
