96f7b4869b98cf681fa354da0b56d813829b0b49
Gayoso Facundo <facusbrr@gmail.com>
2024-08-28 23:54:46 -0300

    Modificando el controlador de task para utilizar pool correctamente

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -2,30 +2,35 @@ import connectionDB from '../databases/task_db.js';
 
 // Obtener todas las tareas
 export const obtenerTasks = async (req, res) => {
+  let connection;
   try {
-    const connection = await connectionDB();
+    // Lógica de la base de datos
+    connection = await connectionDB();
     const [results] = await connection.query('SELECT * FROM tasks');
-    connection.end();
     return res.status(200).json(results);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Error al obtener las tareas' });
+  } finally {
+    if (connection) connection.release();
   }
 };
 
 // Obtener una tarea mediante su id
 export const obtenerTask = async (req, res) => {
+  let connection;
   try {
+    // El ID tiene que ser número
     const id = parseInt(req.params.id);
     if (!id) {
       return res.status(400).send({ message: 'El id tiene que ser un numero' });
     }
-    const connection = await connectionDB();
+    // Lógica de la Base de Datos
+    connection = await connectionDB();
     const [results] = await connection.query(
       'SELECT * FROM tasks WHERE id = ?',
       [id]
     );
-    connection.end();
     if (results.length === 0) {
       return res.status(404).json({ message: 'Tarea no encontrada' });
     }
@@ -35,49 +40,58 @@ export const obtenerTask = async (req, res) => {
     return res
       .status(500)
       .json({ message: 'Error al obtener la tarea específica' });
+  } finally {
+    if (connection) connection.release();
   }
 };
 
 // Crear una nueva tarea
 export const createTasks = async (req, res) => {
+  let connection;
   try {
     const { title, description, isComplete } = req.body;
-    const connection = await connectionDB();
+    // Lógica Base de Datos
+    connection = await connectionDB();
     await connection.query(
       'INSERT INTO TASKS(title, description, isComplete) VALUES (?,?,?)',
       [title, description, isComplete]
     );
-    connection.end();
     return res.status(201).json({ message: 'Tarea creada' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Error al crear la tarea' });
+  } finally {
+    if (connection) connection.release();
   }
 };
 
 // Editar tareas por id
 export const editTask = async (req, res) => {
+  let connection;
   try {
     const id = parseInt(req.params.id);
     const { title, description, isComplete } = req.body;
-    const connection = await connectionDB();
+    // Lógica Base de Datos
+    connection = await connectionDB();
     await connection.query(
       'UPDATE tasks SET title = ?, description = ?, isComplete = ? WHERE id = ?',
       [title, description, isComplete, id]
     );
-    connection.end();
     return res.status(200).json({ message: 'Tarea editada' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'No se pudo editar la tarea' });
+  } finally {
+    if (connection) connection.release();
   }
 };
 
 // Eliminar tareas
 export const deleteTask = async (req, res) => {
+  let connection;
   try {
     const id = parseInt(req.params.id);
-    const connection = await connectionDB();
+    connection = await connectionDB();
     const [results] = await connection.query(
       'SELECT * FROM tasks WHERE id = ?',
       [id]
@@ -86,10 +100,11 @@ export const deleteTask = async (req, res) => {
       return res.status(404).send({ message: 'Tarea no encontrada' });
     }
     await connection.query('DELETE FROM tasks WHERE id = ?', [id]);
-    connection.end();
     return res.status(200).json({ message: 'Tarea eliminada' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Error al eliminar la tarea' });
+  } finally {
+    if (connection) connection.release();
   }
 };
