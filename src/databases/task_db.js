e4b8adf4e9dbebc2c80c76ac2a821e0503881c87
Gayoso Facundo <facusbrr@gmail.com>
2024-08-28 21:39:23 -0300

    Se instalo la dependencia dotenv y se configuro la base de datos

--- src/databases/task_db.js
+++ src/databases/task_db.js
@@ -1,11 +1,13 @@
 import { createConnection } from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   return await createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tasks_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 };
 
